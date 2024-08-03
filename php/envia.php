3ca143d9f00a9305e0839039faf64224796d433b
Pedro Simão <167440279+Simao-dev@users.noreply.github.com>
2024-08-03 10:47:17 -0300

    subi os arquivos do meu portfólio

---
+++ php/envia.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $telefone = addcslashes($_POST['telefone']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $para = "pedrosimaocontato@gmail.com";
+    $assunto = "Coleta de dados - Simão.dev";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: pedimkalleb10@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
+
+?>
